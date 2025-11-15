3945be816b0adc25b74cd5029a5364dbc1d70e59
vukp <pukvavlovic10@gmail.com>
2025-11-15 22:58:44 +0100

    add header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
-    <>
+    <div className="header">
       <h1>Welcome to Memory Card game</h1>
       <p>made with React</p>
-    </>
+    </div>
   );
 }
