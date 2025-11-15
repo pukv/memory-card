a559c144cadae7e11cc5ff6d16deb820f1e3211a
vukp <pukvavlovic10@gmail.com>
2025-11-15 23:03:02 +0100

    cards initialized as an empty array. UseEffect added that handles the fetching of pokemon data. Add handleClick function that logs which id was clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import Card from "./components/Card.jsx";
+import { useEffect, useState } from "react";
 import Header from "./components/Header.jsx";
+import CardGrid from "./components/CardGrid.jsx";
 
 export default function App() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    async function fetchPokemons() {
+      const ids = [1, 4, 7, 25, 39, 52, 133, 150, 196, 212];
+      const promises = ids.map((id) =>
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
+          res.json()
+        )
+      );
+
+      const data = await Promise.all(promises);
+
+      const formatted = data.map((pokemon) => ({
+        id: pokemon.id,
+        name: pokemon.name,
+        image: pokemon.sprites.other["official-artwork"].front_default,
+      }));
+
+      setCards(formatted);
+    }
+    fetchPokemons();
+  }, []);
+
+  function handleCardClick(id) {
+    console.log("clicked", id);
+  }
   return (
     <>
       <Header />
-      <Card />
+      <CardGrid cards={cards} handleCardClick={handleCardClick} />
     </>
   );
 }
